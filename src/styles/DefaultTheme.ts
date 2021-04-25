6648c8295204fb0a74432a8088df208d2e3de35b
silviocoutinho <silviocoutinho@ymail.com>
2021-04-25 17:12:37 -0300

    Add Interface DefaultTheme

---
+++ src/styles/DefaultTheme.ts
@@ -0,0 +1,17 @@
+export interface DefaultTheme {
+    borderRadius: string;
+
+    colors: {
+
+        primary: string;
+        secondary: string;
+        placeholder: string;
+        label: string;
+        background: string;
+        font: string;
+        border: string;
+        error: string;
+        focusedBackgroud: string;
+        focusedBorder: string;
+    };
+}
