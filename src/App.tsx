3025af804efebbc1dcbc01b6b7ed94f1bc98c122
silviocoutinho <silviocoutinho@ymail.com>
2021-04-25 17:14:07 -0300

    Add Example of Input

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
-
+import Input from './components/Input';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
+        <h2>Input</h2>
+        <p>Component Input</p>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <Input
+            fieldName="example1"
+            label="Example"
+            placeholder="Type a text"
+          />
+          <Input
+            fieldName="example2"
+            label="Example with Default Value"
+            placeholder="Type a text"
+            defaultValue="test value"
+          />
+          <Input
+            fieldName="example3"
+            label="Example with Error Message"
+            placeholder="Type a text"
+            errorMessage="Please, type a number"
+          />
         </p>
-        
       </header>
     </div>
   );
