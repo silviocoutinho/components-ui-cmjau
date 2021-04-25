7885a29344bc5ef65bd4c99fe427d52b1dcdb7c3
silviocoutinho <silviocoutinho@ymail.com>
2021-04-25 17:12:53 -0300

    Add theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,21 @@
+import { DefaultTheme } from './DefaultTheme';
+
+const light: DefaultTheme = {
+  borderRadius: '5px',
+
+  colors: {
+    primary: '#ffffcc',
+    secondary: '#ccffcc',
+
+    placeholder: '#bfbfbf',
+    label: '#4f4f4f',
+    background: '#fff',
+    font: '#404040',
+    border: '#d6d7da',
+    error: '#d80e40',
+    focusedBackgroud: '#ffffe6',
+    focusedBorder: '#808080',
+  },
+};
+
+export { light };
