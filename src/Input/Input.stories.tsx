052827e42361835fef5529ab6ab1cbbbc1ad7d92
silviocoutinho <silviocoutinho@ymail.com>
2021-04-29 00:59:09 -0300

    Create Input.stories

---
+++ src/Input/Input.stories.tsx
@@ -0,0 +1,32 @@
+import React, { ComponentProps } from 'react';
+
+import Input from './Input';
+
+export default {
+  title: 'forms/Input',
+  component: Input,
+};
+
+export const Basic = args => <Input {...args} />;
+Basic.args = {
+  fieldName: 'example1',
+  label: 'Example',
+  placeholder: 'Type here',
+};
+
+export const All = () => (
+  <>
+    <Input
+      fieldName="example2"
+      label="Example with Default Value"
+      placeholder="Type a text"
+      defaultValue="Default Value"
+    />
+    <Input
+      fieldName="example3"
+      label="Example with Error Message"
+      placeholder="Type a text"
+      errorMessage="Please, type a number"
+    />
+  </>
+);
