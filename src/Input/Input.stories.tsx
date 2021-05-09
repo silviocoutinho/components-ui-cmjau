3901b0ddced2824611b2fdf314ab95b290af0993
silviocoutinho <silviocoutinho@ymail.com>
2021-05-09 15:47:29 -0300

    Change fieldName to field

--- src/Input/Input.stories.tsx
+++ src/Input/Input.stories.tsx
@@ -10,7 +10,7 @@ export default {
 
 export const Basic = args => <Input {...args} />;
 Basic.args = {
-  fieldName: 'example1',
+  field: 'example1',
   label: 'Example',
   placeholder: 'Type here',
 };
@@ -18,7 +18,7 @@ Basic.args = {
 export const Icon = () => (
   <>
     <Input
-      fieldName="exampleIcon"
+      field="exampleIcon"
       label="Example with Icon"
       placeholder="Type a text"
       icon={FiMail}
@@ -29,17 +29,17 @@ export const Icon = () => (
 export const All = () => (
   <>
     <Input
-      fieldName="example2"
+      field="example2"
       label="Example with Default Value"
       placeholder="Type a text"
       defaultValue="Default Value"
     />
     <Input
-      fieldName="example3"
+      field="example3"
       label="Example with Error Message"
       placeholder="Type a text"
       errorMessage="Please, type a number"
     />
-    <Input fieldName="example4" placeholder="No label" />
+    <Input field="example4" placeholder="No label" />
   </>
 );
