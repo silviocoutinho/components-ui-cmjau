b1eb9937f76204b259fed0979f4962eafc54f241
Silvio Coutinho <silviocoutinho@ymail.com>
2021-05-13 16:45:25 -0300

    Add Month Control

--- src/MonthYearSelect/MonthYear.tsx
+++ src/MonthYearSelect/MonthYear.tsx
@@ -1,27 +1,44 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useState, useRef, useCallback, useEffect } from 'react';
 import { Props } from './MonthYear.types';
 import { Wrapper } from './MonthYear.styles';
 import Button from '../Button/Button';
 
 const MonthYear = ({ startYear, endYear, errorMessage, ...rest }: Props) => {
-  const monthRef = useRef<HTMLInputElement>(null);
-  const yearRef = useRef<HTMLInputElement>(null);
+  const monthRef = useRef<HTMLSelectElement>(null);
+  const yearRef = useRef<HTMLSelectElement>(null);
+
+  const [fieldMonth, setFieldMonth] = useState(0);
+  const [fieldYear, setFieldYear] = useState(endYear);
+  const [controlMonth, setControlMonth] = useState(new Date().getMonth());
 
   const months = new Array(
     'Janeiro',
     'Fevereiro',
-    'Marco',
+    'Março',
     'Abril',
     'Maio',
     'Junho',
     'Julho',
     'Agosto',
-    'Septembro',
+    'Setembro',
     'Outubro',
     'Novembro',
     'Dezembro',
   );
-  const currentMonth = new Date().getMonth();
+
+  const handleYear = evt => {
+    setFieldYear(evt.target.value);
+    setFieldMonth(0);
+    if (evt.target.value == endYear) {
+      setControlMonth(new Date().getMonth());
+    } else {
+      setControlMonth(12);
+    }
+  };
+
+  const handleMonth = evt => {
+    setFieldMonth(evt.target.value);
+  };
 
   let years = [];
 
@@ -34,9 +51,14 @@ const MonthYear = ({ startYear, endYear, errorMessage, ...rest }: Props) => {
       <div className="grid-container">
         <div className="select-container">
           <label>Mês</label>
-          <select id="selectMonths">
+          <select
+            id="selectMonths"
+            ref={monthRef}
+            onChange={handleMonth}
+            value={fieldMonth}
+          >
             {months.map((element, key) => {
-              if (key <= currentMonth) {
+              if (key <= controlMonth) {
                 return <option key={key}>{element}</option>;
               } else {
                 return (
@@ -50,23 +72,19 @@ const MonthYear = ({ startYear, endYear, errorMessage, ...rest }: Props) => {
         </div>
         <div className="select-container">
           <label>Ano</label>
-          <select id="selectYears">
+          <select
+            id="selectYears"
+            ref={yearRef}
+            onChange={handleYear}
+            value={fieldYear}
+          >
             {years.map((element, key) => {
-              if (key <= currentMonth) {
-                return <option key={key}>{element}</option>;
-              } else {
-                return (
-                  <option key={key} disabled>
-                    {element}
-                  </option>
-                );
-              }
+              return <option key={key}>{element}</option>;
             })}
           </select>
         </div>
-        <div className="button-container">
-          <Button type="button" btnStyle="primary" label="Filtrar" />
-        </div>
+
+        <Button type="button" btnStyle="primary" label="Filtrar" />
 
         {errorMessage && (
           <div>
