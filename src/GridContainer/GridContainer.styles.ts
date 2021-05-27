3e55c5698710687cc498f2b2da1fa2b125d4d9d2
Silvio Coutinho <silviocoutinho@ymail.com>
2021-05-27 08:29:26 -0300

    Changing columns patter

--- src/GridContainer/GridContainer.styles.ts
+++ src/GridContainer/GridContainer.styles.ts
@@ -7,7 +7,7 @@ interface Props {
 export const Wrapper = styled.div<Props>`
   font-family: 'Titillium Web', sans-serif;
   display: grid;
-  grid-template-columns: ${props => props ? 'auto '.repeat(props.columns): 'auto auto auto'};
+  grid-template-columns: ${props => props ? 'auto '.repeat(props.columns): 'auto auto'};
   grid-gap: 2rem;
   background-color: transparent;
   padding: 1rem;
